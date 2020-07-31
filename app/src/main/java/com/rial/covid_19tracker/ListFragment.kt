bad38cb0c0009372abfeba0a93c22a9a8f7f6e52
Mateo Rial <mateo.rial@gmail.com>
2020-07-31 15:49:11 -0300

    doing count in fragment

--- app/src/main/java/com/rial/covid_19tracker/ListFragment.kt
+++ app/src/main/java/com/rial/covid_19tracker/ListFragment.kt
@@ -5,11 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import com.rial.covid_19tracker.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,9 +29,21 @@ class ListFragment : Fragment() {
             view.findNavController().navigate(ListFragmentDirections.actionListFragmentToDetailFragment(5))
         }
 
+        var count = 0
+
+        binding.addButton.setOnClickListener{view : View ->
+            count += 1
+            binding.countTextView.text = count.toString()
+        }
+
+
+
+
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+
         return binding.root
 
     }
-
 
 }
---
+++ app/src/main/java/com/rial/covid_19tracker/ListViewModel.kt
@@ -0,0 +1,14 @@
+package com.rial.covid_19tracker
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+
+    init {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
